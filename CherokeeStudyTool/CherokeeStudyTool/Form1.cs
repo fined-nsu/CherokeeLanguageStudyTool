2e0d4f7a6882a244c7808eb1ec259c13951bbd06
fined-nsu <71229695+fined-nsu@users.noreply.github.com>
2021-01-16 14:18:33 -0600

    Initial commit
    Initial commit of repository.

---
+++ CherokeeStudyTool/CherokeeStudyTool/Form1.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CherokeeStudyTool
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void loadSyllabaryForm(object sender, EventArgs e)
+        {
+            Form2 Syllabary = new Form2();
+            Syllabary.ShowDialog();
+        }
+
+        private void Exit(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void loadList()
+        {
+           
+        }
+    }
+}
---
+++ CherokeeStudyTool/CherokeeStudyTool/Form2.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CherokeeStudyTool
+{
+    public partial class Form2 : Form
+    {
+        string[] images = System.IO.Directory.GetFiles(@"C:\Users\fined\Google Drive\School\Capstone\Resources\Images\Syllabary", "*.png");
+        int i = 0;
+        public Form2()
+        {
+            InitializeComponent();
+        }
+
+        private void Exit(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            
+            pictureBox1.Image = new Bitmap(images[i++]);
+            if (i > images.Length - 1)
+            {
+                i = 0;
+            }
+        }
+    }
+}
