154e8abfed44edf4f33fc72324ce5e297df7cd73
fined-nsu <71229695+fined-nsu@users.noreply.github.com>
2021-04-22 08:37:46 -0500

    April 22
    Readme update

--- CherokeeStudyTool/About.Designer.cs
+++ CherokeeStudyTool/About.Designer.cs
@@ -49,11 +49,13 @@ namespace CherokeeLanguageLearningTool
             // menuStrip1
             // 
             this.menuStrip1.BackColor = System.Drawing.Color.SteelBlue;
+            this.menuStrip1.GripMargin = new System.Windows.Forms.Padding(2, 2, 0, 2);
+            this.menuStrip1.ImageScalingSize = new System.Drawing.Size(24, 24);
             this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
             this.fileToolStripMenuItem});
             this.menuStrip1.Location = new System.Drawing.Point(0, 0);
             this.menuStrip1.Name = "menuStrip1";
-            this.menuStrip1.Size = new System.Drawing.Size(1432, 24);
+            this.menuStrip1.Size = new System.Drawing.Size(2148, 36);
             this.menuStrip1.TabIndex = 0;
             this.menuStrip1.Text = "menuStrip1";
             // 
@@ -62,7 +64,7 @@ namespace CherokeeLanguageLearningTool
             this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
             this.returnToMainMenuToolStripMenuItem});
             this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
-            this.fileToolStripMenuItem.Size = new System.Drawing.Size(37, 20);
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(54, 30);
             this.fileToolStripMenuItem.Text = "File";
             // 
             // returnToMainMenuToolStripMenuItem
@@ -70,16 +72,17 @@ namespace CherokeeLanguageLearningTool
             this.returnToMainMenuToolStripMenuItem.BackColor = System.Drawing.Color.SteelBlue;
             this.returnToMainMenuToolStripMenuItem.ForeColor = System.Drawing.Color.White;
             this.returnToMainMenuToolStripMenuItem.Name = "returnToMainMenuToolStripMenuItem";
-            this.returnToMainMenuToolStripMenuItem.Size = new System.Drawing.Size(187, 22);
+            this.returnToMainMenuToolStripMenuItem.Size = new System.Drawing.Size(281, 34);
             this.returnToMainMenuToolStripMenuItem.Text = "Return to Main Menu";
             this.returnToMainMenuToolStripMenuItem.Click += new System.EventHandler(this.ReturnToMainMenu);
             // 
             // buttonClose
             // 
             this.buttonClose.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.buttonClose.Location = new System.Drawing.Point(616, 686);
+            this.buttonClose.Location = new System.Drawing.Point(924, 1055);
+            this.buttonClose.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.buttonClose.Name = "buttonClose";
-            this.buttonClose.Size = new System.Drawing.Size(200, 63);
+            this.buttonClose.Size = new System.Drawing.Size(300, 97);
             this.buttonClose.TabIndex = 1;
             this.buttonClose.Text = "Close";
             this.buttonClose.UseVisualStyleBackColor = true;
@@ -91,9 +94,10 @@ namespace CherokeeLanguageLearningTool
             this.linkLabelCherokeeLanguageSite.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.linkLabelCherokeeLanguageSite.ForeColor = System.Drawing.Color.Black;
             this.linkLabelCherokeeLanguageSite.LinkColor = System.Drawing.Color.SteelBlue;
-            this.linkLabelCherokeeLanguageSite.Location = new System.Drawing.Point(378, 430);
+            this.linkLabelCherokeeLanguageSite.Location = new System.Drawing.Point(567, 662);
+            this.linkLabelCherokeeLanguageSite.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.linkLabelCherokeeLanguageSite.Name = "linkLabelCherokeeLanguageSite";
-            this.linkLabelCherokeeLanguageSite.Size = new System.Drawing.Size(345, 20);
+            this.linkLabelCherokeeLanguageSite.Size = new System.Drawing.Size(526, 29);
             this.linkLabelCherokeeLanguageSite.TabIndex = 2;
             this.linkLabelCherokeeLanguageSite.TabStop = true;
             this.linkLabelCherokeeLanguageSite.Text = "Cherokee Nation\'s Cherokee language program";
@@ -104,11 +108,12 @@ namespace CherokeeLanguageLearningTool
             this.textBox1.BackColor = System.Drawing.Color.LightGray;
             this.textBox1.BorderStyle = System.Windows.Forms.BorderStyle.None;
             this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.textBox1.Location = new System.Drawing.Point(206, 108);
+            this.textBox1.Location = new System.Drawing.Point(309, 166);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.textBox1.Multiline = true;
             this.textBox1.Name = "textBox1";
             this.textBox1.ReadOnly = true;
-            this.textBox1.Size = new System.Drawing.Size(1020, 85);
+            this.textBox1.Size = new System.Drawing.Size(1530, 131);
             this.textBox1.TabIndex = 3;
             this.textBox1.TabStop = false;
             this.textBox1.Text = resources.GetString("textBox1.Text");
@@ -119,9 +124,10 @@ namespace CherokeeLanguageLearningTool
             this.linkLabelGitHubRepository.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.linkLabelGitHubRepository.ForeColor = System.Drawing.Color.Black;
             this.linkLabelGitHubRepository.LinkColor = System.Drawing.Color.SteelBlue;
-            this.linkLabelGitHubRepository.Location = new System.Drawing.Point(555, 597);
+            this.linkLabelGitHubRepository.Location = new System.Drawing.Point(832, 918);
+            this.linkLabelGitHubRepository.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.linkLabelGitHubRepository.Name = "linkLabelGitHubRepository";
-            this.linkLabelGitHubRepository.Size = new System.Drawing.Size(322, 20);
+            this.linkLabelGitHubRepository.Size = new System.Drawing.Size(483, 29);
             this.linkLabelGitHubRepository.TabIndex = 4;
             this.linkLabelGitHubRepository.TabStop = true;
             this.linkLabelGitHubRepository.Text = "GitHub link for those interested in the details";
@@ -132,9 +138,10 @@ namespace CherokeeLanguageLearningTool
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label1.ForeColor = System.Drawing.Color.Black;
-            this.label1.Location = new System.Drawing.Point(206, 320);
+            this.label1.Location = new System.Drawing.Point(309, 492);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(172, 20);
+            this.label1.Size = new System.Drawing.Size(261, 29);
             this.label1.TabIndex = 5;
             this.label1.Text = "Created by: Derek Fine";
             // 
@@ -143,9 +150,10 @@ namespace CherokeeLanguageLearningTool
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label2.ForeColor = System.Drawing.Color.Black;
-            this.label2.Location = new System.Drawing.Point(682, 279);
+            this.label2.Location = new System.Drawing.Point(1023, 429);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(68, 24);
+            this.label2.Size = new System.Drawing.Size(108, 33);
             this.label2.TabIndex = 6;
             this.label2.Text = "Credits";
             // 
@@ -154,9 +162,10 @@ namespace CherokeeLanguageLearningTool
             this.label3.AutoSize = true;
             this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label3.ForeColor = System.Drawing.Color.Black;
-            this.label3.Location = new System.Drawing.Point(206, 398);
+            this.label3.Location = new System.Drawing.Point(309, 612);
+            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(280, 20);
+            this.label3.Size = new System.Drawing.Size(421, 29);
             this.label3.TabIndex = 5;
             this.label3.Text = "Capstone Advisor: Dr. Ernst Bekkering";
             // 
@@ -165,21 +174,24 @@ namespace CherokeeLanguageLearningTool
             this.label4.AutoSize = true;
             this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label4.ForeColor = System.Drawing.Color.Black;
-            this.label4.Location = new System.Drawing.Point(206, 462);
+            this.label4.Location = new System.Drawing.Point(309, 711);
+            this.label4.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(1107, 20);
+            this.label4.Size = new System.Drawing.Size(1688, 29);
             this.label4.TabIndex = 5;
             this.label4.Text = "Beta Testers: Brandon Thompson, Tiffany Muskrat, Mandy Tucker, Tomi Kirkpatrick, " +
     "Jessica Soap, my fellow capstone classmates, Kari Fine, and my children";
+            this.label4.Visible = false;
             // 
             // label5
             // 
             this.label5.AutoSize = true;
             this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label5.ForeColor = System.Drawing.Color.Black;
-            this.label5.Location = new System.Drawing.Point(647, 357);
+            this.label5.Location = new System.Drawing.Point(970, 549);
+            this.label5.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(139, 24);
+            this.label5.Size = new System.Drawing.Size(215, 33);
             this.label5.TabIndex = 7;
             this.label5.Text = "Special Thanks";
             // 
@@ -188,18 +200,19 @@ namespace CherokeeLanguageLearningTool
             this.label6.AutoSize = true;
             this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label6.ForeColor = System.Drawing.Color.Black;
-            this.label6.Location = new System.Drawing.Point(206, 430);
+            this.label6.Location = new System.Drawing.Point(309, 662);
+            this.label6.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label6.Name = "label6";
-            this.label6.Size = new System.Drawing.Size(166, 20);
+            this.label6.Size = new System.Drawing.Size(248, 29);
             this.label6.TabIndex = 5;
             this.label6.Text = "Language Resources:";
             // 
             // About
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.LightGray;
-            this.ClientSize = new System.Drawing.Size(1432, 761);
+            this.ClientSize = new System.Drawing.Size(2148, 1171);
             this.Controls.Add(this.label5);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label6);
@@ -214,6 +227,7 @@ namespace CherokeeLanguageLearningTool
             this.ForeColor = System.Drawing.Color.SteelBlue;
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.MainMenuStrip = this.menuStrip1;
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.Name = "About";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "About";
